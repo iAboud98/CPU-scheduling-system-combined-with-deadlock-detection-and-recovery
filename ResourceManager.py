84f58d3ce9fd2d35149886a1065f1ea11ca51d6f
iAboud98 <aboudfialah98@gmail.com>
2025-01-09 23:39:19 +0200

    fix assign method in ResourceManager

--- ResourceManager.py
+++ ResourceManager.py
@@ -8,13 +8,18 @@ class ResourceManager:
     def request_resource(self, resource_number):
         for resource in self.resources_list:
             if resource.resource_number == resource_number:
-                return resource.available
+                return resource
         return None
 
     def add_resource(self, resource_num):
         new_resource = Resource(resource_num)
         self.resources_list.append(new_resource)
 
-    def assign_resource(self, resource_num):
+    def assign_resource(self, resource_num, pid):
         resource = self.request_resource(resource_num)
-        resource.assign_resource(resource_num)
+        resource.assign_resource(pid)
+
+    def print_resources(self):
+        for resource in self.resources_list:
+            print(f"resource number -> {resource.resource_number}, availability -> {resource.available}, current "
+                  f"Process -> {resource.current_attach}")
