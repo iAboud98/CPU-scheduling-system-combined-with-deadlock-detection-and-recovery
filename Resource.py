c09c22e9fe5c76b413daeb2e46fa4b01fa2c27e8
iAboud98 <aboudfialah98@gmail.com>
2025-01-13 23:50:40 +0200

    add comments to Resource.py

--- Resource.py
+++ Resource.py
@@ -1,21 +1,21 @@
-class Resource:
-    def __init__(self, resource_number):
-        self.resource_number = resource_number
-        self.available = True
-        self.current_attach = None
+class Resource:  # -> Resource class
+    def __init__(self, resource_number):  # -> Instructor method for resource class to save attributes
+        self.resource_number = resource_number  # -> Resource number
+        self.available = True  # -> set the initial value to True for available attribute
+        self.current_attach = None  # -> current attach to save process id, set initial value to None
 
-    def __eq__(self, other):
+    def __eq__(self, other):  # -> built in method to make the equality based on resource number not on memory address
         if isinstance(other, Resource):
             return self.resource_number == other.resource_number
         return False
 
-    def is_available(self):
+    def is_available(self):  # -> return availability of resource
         return self.available
 
-    def assign_resource(self, pid):
+    def assign_resource(self, pid):  # -> assign resource
         self.available = False
         self.current_attach = pid
 
-    def free_resource(self):
+    def free_resource(self):  # -> free resource
         self.available = True
         self.current_attach = None
