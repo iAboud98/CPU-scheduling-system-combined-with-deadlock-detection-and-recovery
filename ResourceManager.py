6a0d7ae05c7fd13524c9941974f033a2e847f788
iAboud98 <aboudfialah98@gmail.com>
2025-01-14 00:34:53 +0200

    add comments to ResourceManager.py

--- ResourceManager.py
+++ ResourceManager.py
@@ -1,34 +1,34 @@
 from Resource import Resource
 
 
-class ResourceManager:
-    def __init__(self):
+class ResourceManager:  # -> class made to manage resource operations
+    def __init__(self):  # -> instructor to create the list of resources
         self.resources_list = []
 
-    def request_resource(self, resource_number):
+    def request_resource(self, resource_number):  # -> return resource instance if created before, None if not
         for resource in self.resources_list:
             if resource.resource_number == resource_number:
                 return resource
         return None
 
-    def add_resource(self, resource_num):
+    def add_resource(self, resource_num):  # -> create resource instance with given resource num and add it to list
         new_resource = Resource(resource_num)
         self.resources_list.append(new_resource)
 
-    def assign_resource(self, resource_num, pid):
+    def assign_resource(self, resource_num, pid):  # -> assign resource to process and make it unavailable
         resource = self.request_resource(resource_num)
         resource.assign_resource(pid)
 
-    def free_resource(self, resource_num):
+    def free_resource(self, resource_num):  # -> free resource and change its availability
         resource = self.request_resource(resource_num)
         resource.free_resource()
 
-    def release_all_resources(self, pid):
+    def release_all_resources(self, pid):  # -> free all resources
         for resource in self.resources_list:
             if resource.current_attach == pid:
                 self.free_resource(resource.resource_number)
 
-    def print_resources(self):
+    def print_resources(self):  # -> print resources list
         for resource in self.resources_list:
             print(f"resource number -> {resource.resource_number}, availability -> {resource.available}, current "
                   f"Process -> {resource.current_attach}")
