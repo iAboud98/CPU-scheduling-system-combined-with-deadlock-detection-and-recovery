4cec7380529538f6a84bc1d039fef2ea6648fb55
iAboud98 <aboudfialah98@gmail.com>
2025-01-13 23:29:38 +0200

    add comments to Process.py

--- Process.py
+++ Process.py
@@ -1,21 +1,21 @@
-class Process:
+class Process:  # -> Process class made to save attributes, print them and to apply functions on process
 
     def __init__(self, pid, arrival_time, priority, sequence):
-        self.pid = pid
-        self.arrival_time = arrival_time
-        self.priority = priority
-        self.sequence = sequence
+        self.pid = pid  # -> Process ID
+        self.arrival_time = arrival_time  # -> Arrival Time
+        self.priority = priority  # -> Priority
+        self.sequence = sequence  # -> Sequence
 
-    def print_process(self):
+    def print_process(self):  # -> Method to print process attributes
         print(f"Process ID: {self.pid}, Arrival Time: {self.arrival_time}, Priority: {self.priority}, Sequence: "
               f"{self.sequence}")
 
-    def analyze_input(self):
+    def analyze_input(self):  # -> Method to analyze the first index in process sequence
         if 'R' in self.sequence[0]['bursts'][0]:
-            operation = 'request'
+            operation = 'request'  # -> request operation like : R[1], R[2], R[.....]
         else:
-            operation = 'free'
-        number = int((self.sequence[0]['bursts'][0].split('[')[1]).split(']')[0])
+            operation = 'free'  # -> free operation like : F[1], F[2], F[......]
+        number = int((self.sequence[0]['bursts'][0].split('[')[1]).split(']')[0])  # -> to get the numeric value inside
 
-        return operation, number
+        return operation, number  # -> return both operation and number
 
